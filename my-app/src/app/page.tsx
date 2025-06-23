affbbc6e04de928ad5959b22c27056eb69eed2d4
chang313 <pch313@gmail.com>
2025-06-23 23:58:05 +0900

    chore: install button, calendar components

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
 
 
 export default function Home() {
+  const [date, setDate] = useState<Date | undefined>(undefined);
+
   return (
     <>
-
+    <Calendar mode="single" selected={date} onSelect={setDate} />
     </>
 
   );
