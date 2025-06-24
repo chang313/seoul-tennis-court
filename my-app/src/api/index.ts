51a682bd10b4e7e9dbaccb1c46be3b34f961ca8f
chang313 <pch313@gmail.com>
2025-06-24 23:18:24 +0900

    feat: change data count to max

--- my-app/src/api/index.ts
+++ my-app/src/api/index.ts
@@ -5,7 +5,7 @@ const API_KEY = process.env.NEXT_PUBLIC_OPEN_API_KEY
 
 
 export const getCourtInfoList = async () => {
-  const response = await axios.get(`${BASE_URL}/${API_KEY}/json/ListPublicReservationSport/1/5/테니스장`, {
+  const response = await axios.get(`${BASE_URL}/${API_KEY}/json/ListPublicReservationSport/1/365/테니스장`, {
     headers: {
       'Content-Type': 'application/json',
     }
